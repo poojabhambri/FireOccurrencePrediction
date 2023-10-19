697a2de1ff800fcbc70c517da2d7f8120910149a
aridge03 <94921692+aridge03@users.noreply.github.com>
2023-10-19 14:27:05 -0600

    Update Results.py

--- pages/Results.py
+++ pages/Results.py
@@ -7,6 +7,7 @@ if 'show_outputs' not in st.session_state:
 
 def show_outputs(folder_path):
     png_files = get_png_files_in_folder(folder_path)
+
     if png_files:
         if st.session_state.use_range:
             start_date, end_date = st.session_state.selected_date, st.session_state.end_date
@@ -15,30 +16,52 @@ def show_outputs(folder_path):
 
             selected_dates = st.multiselect("Select dates", date_list, default=date_list)
             selected_files = [file for file in png_files if any(str(date) in file for date in selected_dates)]
-         
-            for idx, png_file in enumerate(selected_files):
-                with open(os.path.join(folder_path, png_file), 'rb') as f:
-                    png_image = f.read()
-                    st.image(png_image, caption=png_file, use_column_width=True)
-                    download_button_key = f"download_button_{idx}"
-                    st.download_button(
-                        label=f"Download {png_file}",
-                        data=png_image,
-                        file_name=png_file,
-                        key=download_button_key,
-                    )
-        else: 
-            for idx, png_file in enumerate(png_files):
-                with open(os.path.join(folder_path, png_file), 'rb') as f:
-                    png_image = f.read()
-                    st.image(png_image, caption=png_file, use_column_width=True)
-                    download_button_key = f"download_button_{idx}"
-                    st.download_button(
-                        label=f"Download {png_file}",
-                        data=png_image,
-                        file_name=png_file,
-                        key=download_button_key,
-                    )
+
+        columns = st.columns(2)
+
+        for idx, file_name in enumerate(selected_files):
+            with open(os.path.join(folder_path, file_name), 'rb') as f:
+                png_image = f.read()
+                png_width = 300  # Default width for files without specific conditions
+                if file_name.startswith("ltg_arrival"):
+                    png_width = 350
+                if file_name.startswith("ltg_dc"):
+                    png_width = 305
+                if file_name.startswith("ltg_dmc"):
+                    png_width = 305
+                if file_name.startswith("ltg_holdover"):
+                    png_width = 360
+                if file_name.startswith("ltg_probarr0"):
+                    png_width = 350
+                if file_name.startswith("ltg_probign"):
+                    png_width = 360
+                if file_name.startswith("ltg_totltg"):
+                    png_width = 350
+                if file_name.startswith("hmn_ffmc"):
+                    png_width=356
+                if file_name.startswith("hmn_probability"):
+                    png_width=395
+                if idx % 2 == 0:
+                    with columns[0]:
+                        st.image(png_image, caption=file_name, width=png_width)
+                        download_button_key = f"download_button_{idx}"
+                        st.download_button(
+                            label=f"Download {file_name}",
+                            data=png_image,
+                            file_name=file_name,
+                            key=download_button_key,
+                        )
+                else:
+                    with columns[1]:
+                        st.image(png_image, caption=file_name, width=png_width)
+                        download_button_key = f"download_button_{idx}"
+                        st.download_button(
+                            label=f"Download {file_name}",
+                            data=png_image,
+                            file_name=file_name,
+                            key=download_button_key,
+                        )
+
 def get_png_files_in_folder(folder_path):
     png_files = [f for f in os.listdir(folder_path) if f.lower().endswith('.png')]
     return png_files
@@ -47,6 +70,7 @@ def main():
     if st.session_state.show_outputs:
         show_outputs("intermediate_output")
     else:
-        st.write("Run the model first to see results")    
+        st.write("Run the model first to see results")
+
 if __name__ == '__main__':
     main()
