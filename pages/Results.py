99a85dba4d3de90727b5d8f8fbf078159f0ecc53
poojabhambri <114958796+poojabhambri@users.noreply.github.com>
2023-10-19 09:51:30 -0600

    add slider for date range

--- pages/Results.py
+++ pages/Results.py
@@ -4,27 +4,38 @@ import os
 def show_outputs(folder_path):
     png_files = get_png_files_in_folder(folder_path)
     if png_files:
-        options = st.multiselect(
-        'Which map(s) do you want displayed?',
-        png_files)
-        st.session_state.options = options
-        show(folder_path)
-    else:
-        st.write("No output created.")
-
-
-def show(folder_path):
-    for idx, png_file in enumerate(st.session_state.options):
-        with open(os.path.join(folder_path, png_file), 'rb') as f:
-            png_image = f.read()
-            st.image(png_image, caption=png_file, use_column_width=True)
-            download_button_key = f"download_button_{idx}"
-            st.download_button(
-                label=f"Download {png_file}",
-                data=png_image,
-                file_name=png_file,
-                key=download_button_key,
-            )
+        if st.session_state.use_range:
+            date = st.slider(
+            "Which date would you like to see?",
+            min_value = st.session_state.selected_date,
+            value= st.session_state.selected_date,
+            max_value= st.session_state.end_date,
+            format="MM/DD/YY")
+            st.write("Start time:", date)
+            selected_files = [file for file in png_files if str(date) in file]
+            for idx, png_file in enumerate(selected_files):
+                with open(os.path.join(folder_path, png_file), 'rb') as f:
+                    png_image = f.read()
+                    st.image(png_image, caption=png_file, use_column_width=True)
+                    download_button_key = f"download_button_{idx}"
+                    st.download_button(
+                        label=f"Download {png_file}",
+                        data=png_image,
+                        file_name=png_file,
+                        key=download_button_key,
+                    )
+        else: 
+            for idx, png_file in enumerate(png_files):
+                with open(os.path.join(folder_path, png_file), 'rb') as f:
+                    png_image = f.read()
+                    st.image(png_image, caption=png_file, use_column_width=True)
+                    download_button_key = f"download_button_{idx}"
+                    st.download_button(
+                        label=f"Download {png_file}",
+                        data=png_image,
+                        file_name=png_file,
+                        key=download_button_key,
+                    )
 def get_png_files_in_folder(folder_path):
     png_files = [f for f in os.listdir(folder_path) if f.lower().endswith('.png')]
     return png_files
