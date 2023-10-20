22a65218c91ff627dbbd499748632ec95a1c83f3
poojabhambri <114958796+poojabhambri@users.noreply.github.com>
2023-10-20 08:51:53 -0600

    Slider and selectbox for ltg

--- pages/Results.py
+++ pages/Results.py
@@ -14,26 +14,27 @@ def show_outputs(folder_path):
         if st.session_state.use_range:
             if st.session_state.model == "Human":
                 choice = st.selectbox("Which map?", ("FFMC", "Probability"))
-                date = st.slider(
-                "Which date would you like to see?",
-                min_value = st.session_state.selected_date,
-                value= st.session_state.selected_date,
-                max_value= st.session_state.end_date,
-                format="MM/DD/YY")
-                selected_files = [file for file in png_files if str(date) in file]
-                png_files = [file for file in selected_files if choice.lower() in file]
-                png_file = png_files[0]
-                with open(os.path.join(folder_path, png_file), 'rb') as f:
-                    png_image = f.read()
-                    st.image(png_image, caption=png_file, use_column_width=True)
-                    download_button_key = f"download_button_1"
-                    st.download_button(
-                        label=f"Download {png_file}",
-                        data=png_image,
-                        file_name=png_file,
-                        key=download_button_key,
-                    )
-
+            elif st.session_state.model == "Lightning":
+                choice = st.selectbox("Which map?", ("Arrival", "DC", "DMC", "Probarr0", "Probign", "Holdover", "Totltg"))
+            date = st.slider(
+            "Which date would you like to see?",
+            min_value = st.session_state.selected_date,
+            value= st.session_state.selected_date,
+            max_value= st.session_state.end_date,
+            format="MM/DD/YY")
+            selected_files = [file for file in png_files if str(date) in file]
+            png_files = [file for file in selected_files if choice.lower() in file]
+            png_file = png_files[0]
+            with open(os.path.join(folder_path, png_file), 'rb') as f:
+                png_image = f.read()
+                st.image(png_image, caption=png_file, use_column_width=True)
+                download_button_key = f"download_button_1"
+                st.download_button(
+                    label=f"Download {png_file}",
+                    data=png_image,
+                    file_name=png_file,
+                    key=download_button_key,
+                )
         else: 
             for idx, png_file in enumerate(png_files):
                 with open(os.path.join(folder_path, png_file), 'rb') as f:
@@ -46,6 +47,7 @@ def show_outputs(folder_path):
                         file_name=png_file,
                         key=download_button_key,
                     )
+            
 def get_png_files_in_folder(folder_path):
     png_files = [f for f in os.listdir(folder_path) if f.lower().endswith('.png')]
     return png_files
