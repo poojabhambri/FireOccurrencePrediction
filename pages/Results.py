d1b9365840c37d4f076ea9bb8698ce0e586abd9b
poojabhambri <114958796+poojabhambri@users.noreply.github.com>
2023-11-01 12:31:26 -0600

    Dropdown for single date prediction

--- pages/Results.py
+++ pages/Results.py
@@ -11,11 +11,11 @@ if 'prob' not in st.session_state:
 def show_outputs(folder_path):
     png_files = get_png_files_in_folder(folder_path)
     if png_files:
-        if st.session_state.use_range:
-            if st.session_state.model == "Human":
-                choice = st.selectbox("Which map?", ("FFMC", "Probability"))
-            elif st.session_state.model == "Lightning":
+        if st.session_state.model == "Human":
+            choice = st.selectbox("Which map?", ("FFMC", "Probability"))
+        elif st.session_state.model == "Lightning":
                 choice = st.selectbox("Which map?", ("Arrival", "DC", "DMC", "Probarr0", "Probign", "Holdover", "Totltg"))
+        if st.session_state.use_range:
             date = st.slider(
             "Which date would you like to see?",
             min_value = st.session_state.selected_date,
@@ -36,7 +36,8 @@ def show_outputs(folder_path):
                     key=download_button_key,
                 )
         else: 
-            for idx, png_file in enumerate(png_files):
+            selected_files = [file for file in png_files if choice.lower() in file]
+            for idx, png_file in enumerate(selected_files):
                 with open(os.path.join(folder_path, png_file), 'rb') as f:
                     png_image = f.read()
                     st.image(png_image, caption=png_file, use_column_width=True)
